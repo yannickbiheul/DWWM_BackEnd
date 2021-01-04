2944e108b9c346e17e00749b4bbe964da38cec88
Your Name <yannickbiheul@outlook.fr>
2021-01-04 15:42:51 +0100

    Création d'objets

--- Objets/classes/utilisateur.class.php
+++ Objets/classes/utilisateur.class.php
@@ -1,7 +1,20 @@
 <?php
 
     class Utilisateur {
-        
+        private $user_name;
+        private $user_pass;
+
+        public function getNom() {
+            return $this->user_name;
+        }
+
+        public function setNom($new_user_name) {
+            $this->user_name = $new_user_name;
+        }
+
+        public function setPasse($new_user_pass) {
+            $this->user_pass = $new_user_pass;
+        }
     };
 
 ?>
--- Objets/index.php
+++ Objets/index.php
@@ -1,8 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Objets !</title>
+</head>
+<body>
+    <h1>Titre Principal</h1>
+
+    <?php
 
     require 'classes/utilisateur.class.php';
 
     $pierre = new Utilisateur();
+    $mathilde = new Utilisateur();
 
+    $pierre->setNom('Pierre');
+    $pierre->setPasse('abcdef');
 
-?>
+    $mathilde->setNom('Math');
+    $mathilde->setPasse(123456);
+
+    echo $pierre->getNom() . '<br>';
+    echo $mathilde->getNom() . '<br>';
+
+    ?>
+
+    <p>Un paragraphe</p>
+</body>
+</html>
+

1e1291bca4962f7b65ff17205eedb9878eb1bb61
Your Name <yannickbiheul@outlook.fr>
2021-01-04 16:50:14 +0100

    Modifs

--- Objets/index.php
+++ Objets/index.php
@@ -5,24 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Objets !</title>
 </head>
-<body>
+<body style="display:flex; flex-direction:column; justify-content: center; align-items: center;">
     <h1>Titre Principal</h1>
+    <form action="index.php" method="POST">
+        <input type="text" name="nom" id="nom" placeholder="Nom" style="margin:10px 0;"><br>
+        <input type="password" name="pass" id="pass" placeholder="Mot de passe" style="margin:10px 0;"><br>
+        <input type="submit" value="Envoyer" style="margin:10px 0;">
+    </form>
 
     <?php
 
     require 'classes/utilisateur.class.php';
 
-    $pierre = new Utilisateur();
-    $mathilde = new Utilisateur();
+    
 
-    $pierre->setNom('Pierre');
-    $pierre->setPasse('abcdef');
-
-    $mathilde->setNom('Math');
-    $mathilde->setPasse(123456);
-
-    echo $pierre->getNom() . '<br>';
-    echo $mathilde->getNom() . '<br>';
+    if (isset($_POST['nom']) && isset($_POST['pass'])) {
+        $pierre = new Utilisateur($_POST['nom'], $_POST['pass']);
+        echo "<p style='color:green; margin:40px; font-size:2em;'>" . $pierre->getNom() . '</p><br>';
+    } else {
+        echo "";
+    }
+    
 
     ?>
 
